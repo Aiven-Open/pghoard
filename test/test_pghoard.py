fc01356ea5c4edf87e917f7aa9eadf832cb775a6
Hannu Valtonen <hannu.valtonen@ohmu.fi>
2015-02-25 02:28:49 +0200

    tests: Add a couple of more unittests

--- test/test_pghoard.py
+++ test/test_pghoard.py
@@ -21,6 +21,8 @@ def create_json_conf(filepath, temp_dir):
             },
         },
         "backup_location": temp_dir,
+        "alert_file_dir": temp_dir,
+        "json_state_file_path": temp_dir + "/state.json",
     }
     with open(filepath, "w") as fp:
         json.dump(conf, fp)
@@ -34,6 +36,33 @@ class TestPGHoard(TestCase):
         self.xlog_path = os.path.join(self.temp_dir, "default", "xlog")
         os.makedirs(self.xlog_path)
         self.pghoard = PGHoard(config_path)
+
+    def test_alert_files(self):
+        alert_file_path = os.path.join(self.temp_dir, "test_alert")
+        self.pghoard.create_alert_file("test_alert")
+        self.assertTrue(os.path.exists(alert_file_path))
+        self.pghoard.delete_alert_file("test_alert")
+        self.assertFalse(os.path.exists(alert_file_path))
+
+    def test_backup_state_file(self):
+        self.pghoard.write_backup_state_to_json_file()
+        state_path = os.path.join(self.temp_dir, "state.json")
+        with open(state_path, "r") as fp:
+            state = json.load(fp)
+        empty_state = {
+            "startup_time": self.pghoard.state["startup_time"],
+            "backup_clusters": {},
+            "compressors": [{}, {}],
+            "queues": {
+                "compression_queue": 0,
+                "transfer_queue": 0,
+            },
+            "data_transfer": {},
+            "transfer_agents": [{}, {}],
+            "pg_receivexlogs": {},
+            "pg_basebackups": {},
+        }
+        assert empty_state == state
 
     def test_startup_walk_for_missed_files(self):
         with open(os.path.join(self.xlog_path, "000000010000000000000004"), "wb") as fp:
