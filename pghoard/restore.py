f8d40c6ad26d62ef6e7ccb531c33e10a9d911109
Hannu Valtonen <hannu.valtonen@ohmu.fi>
2015-05-06 15:44:22 +0300

    restore: By default restore to a standby, the user can promote the DB if needed
    This may need to be made configurable along with setting of recovery target
    and primary_conn_info. Also pass along an optional primary_conninfo if
    we want the new standby to replicate from somewhere.

--- pghoard/restore.py
+++ pghoard/restore.py
@@ -33,12 +33,13 @@ def create_pgdata_dir(pgdata):
     os.chmod(pgdata, 0o700)
 
 
-def create_recovery_conf(dirpath, site):
+def create_recovery_conf(dirpath, site, primary_conninfo):
     content = """# pghoard created recovery.conf
+standby_mode = 'on'
+primary_conninfo = {}
 restore_command = 'pghoard_restore get %f %p --site {}'
-recovery_target = 'immediate'
 recovery_target_timeline = 'latest'
-""".format(site)
+""".format(primary_conninfo, site)
     filepath = os.path.join(dirpath, "recovery.conf")
     with open(filepath, "w") as fp:
         fp.write(content)
@@ -74,10 +75,11 @@ class Restore(object):
     @argh.arg("--port", help="pghoard repository port")
     @argh.arg("--site", help="pghoard site")
     @argh.arg("--basebackup", help="pghoard basebackup")
+    @argh.arg("--primary-conninfo", help="replication.conf primary_conninfo")
     @argh.arg("--target-dir", help="pghoard restore target 'pgdata' dir", required=True)
-    def get_basebackup_http(self, target_dir, basebackup="latest", host="localhost", port=16000, site="default"):
+    def get_basebackup_http(self, target_dir, primary_conninfo="", basebackup="latest", host="localhost", port=16000, site="default"):
         self.storage = HTTPRestore(host, port, site, target_dir)
-        self.get_basebackup(target_dir, basebackup, site)
+        self.get_basebackup(target_dir, basebackup, site, primary_conninfo)
 
     @argh.arg("--host", help="pghoard repository host")
     @argh.arg("--port", help="pghoard repository port")
@@ -92,10 +94,11 @@ class Restore(object):
     @argh.arg("--bucket", help="AWS S3 bucket name", required=True)
     @argh.arg("--site", help="pghoard site")
     @argh.arg("--basebackup", help="pghoard basebackup")
+    @argh.arg("--primary-conninfo", help="replication.conf primary_conninfo")
     @argh.arg("--target-dir", help="pghoard restore target 'pgdata' dir", required=True)
-    def get_basebackup_s3(self, aws_access_key_id, aws_secret_access_key, bucket, target_dir, basebackup="latest", region="eu-west-1", site="default"):
+    def get_basebackup_s3(self, aws_access_key_id, aws_secret_access_key, bucket, target_dir, primary_conninfo="", basebackup="latest", region="eu-west-1", site="default"):
         self.storage = S3Restore(aws_access_key_id, aws_secret_access_key, region, bucket, site, target_dir)
-        self.get_basebackup(target_dir, basebackup, site)
+        self.get_basebackup(target_dir, basebackup, site, primary_conninfo)
 
     @argh.arg("--aws-access-key-id", help="AWS Access Key ID [AWS_ACCESS_KEY_ID]", default=os.environ.get("AWS_ACCESS_KEY_ID"))
     @argh.arg("--aws-secret-access-key", help="AWS Secret Access Key [AWS_SECRET_ACCESS_KEY]", default=os.environ.get("AWS_SECRET_ACCESS_KEY"))
@@ -104,9 +107,8 @@ class Restore(object):
     @argh.arg("--site", help="pghoard site")
     def list_basebackups_s3(self, aws_access_key_id, aws_secret_access_key, bucket, region="eu-west-1", site="default"):
         self.storage = S3Restore(aws_access_key_id, aws_secret_access_key, region, bucket, site)
-        self.storage.show_basebackup_list()
 
-    def get_basebackup(self, pgdata, basebackup, site):
+    def get_basebackup(self, pgdata, basebackup, site, primary_conninfo):
         #  If basebackup that we want it set as latest, figure out which one it is
         if basebackup == "latest":
             basebackups = self.storage.list_basebackups()  # pylint: disable=protected-access
@@ -120,7 +122,7 @@ class Restore(object):
         _, tar = self.storage.get_basebackup_file(basebackup)
         tar.extractall(pgdata)
 
-        create_recovery_conf(pgdata, site)
+        create_recovery_conf(pgdata, site, primary_conninfo)
 
         print("Basebackup complete.")
         print("You can start PostgreSQL by running pg_ctl -D %s start" % pgdata)
