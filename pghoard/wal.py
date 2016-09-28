3188a64d9ee219d19a7130cdb413827e9f46f858
Oskari Saarenmaa <os@ohmu.fi>
2016-09-28 12:49:44 +0300

    wal.py: drop the now obsolete beta comment in 9.6 wal magic line

--- pghoard/wal.py
+++ pghoard/wal.py
@@ -18,7 +18,7 @@ WAL_MAGIC = {
     0xD075: 90300,
     0xD07E: 90400,
     0xD087: 90500,
-    0xD093: 90600,  # 9.6beta3
+    0xD093: 90600,
 }
 WAL_MAGIC_BY_VERSION = {value: key for key, value in WAL_MAGIC.items()}
 
