34a9a092cdc95c1ff9d59d59b732ff0971fbd26b
Hannu Valtonen <hannu.valtonen@ohmu.fi>
2016-11-11 16:19:31 +0200

    pghoard: Use PGPASSWORD env variable to hide passwords from process listing

--- pghoard/receivexlog.py
+++ pghoard/receivexlog.py
@@ -11,17 +11,17 @@ import select
 import subprocess
 import time
 
-from .common import set_subprocess_stdout_and_stderr_nonblocking, terminate_subprocess
-from .pgutil import get_connection_info
+from .common import (replication_connection_string_and_slot_for_node, set_subprocess_stdout_and_stderr_nonblocking,
+                     terminate_subprocess)
 from threading import Thread
 
 
 class PGReceiveXLog(Thread):
-    def __init__(self, config, connection_string, xlog_location, site, slot, pg_version_server):
+    def __init__(self, config, connection_info, xlog_location, site, slot, pg_version_server):
         super().__init__()
         self.log = logging.getLogger("PGReceiveXLog")
         self.config = config
-        self.connection_string = connection_string
+        self.connection_info = connection_info
         self.xlog_location = xlog_location
         self.site = site
         self.slot = slot
@@ -40,23 +40,33 @@ class PGReceiveXLog(Thread):
             "--verbose",
             "--directory", self.xlog_location,
         ]
+
+        # May be empty if using trust auth
+        password = self.connection_info.pop("password", "")
         if self.pg_version_server < 90300:
-            conn_info = get_connection_info(self.connection_string)
-            if "user" in conn_info:
-                command.extend(["--user", conn_info["user"]])
-            if "port" in conn_info:
-                command.extend(["--port", conn_info["port"]])
-            if "host" in conn_info:
-                command.extend(["--host", conn_info["host"]])
+            if "user" in self.connection_info:
+                command.extend(["--user", self.connection_info["user"]])
+            if "port" in self.connection_info:
+                command.extend(["--port", self.connection_info["port"]])
+            if "host" in self.connection_info:
+                command.extend(["--host", self.connection_info["host"]])
         else:
-            command.extend(["--dbname", self.connection_string])
+            connection_string, _ = replication_connection_string_and_slot_for_node(self.connection_info)
+            command.extend(["--dbname", connection_string])
 
         if self.pg_version_server >= 90400 and self.slot:
             command.extend(["--slot", self.slot])
 
         self.log.debug("Starting to run: %r", command)
         start_time = time.time()
-        proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        env = {}
+        if password:
+            env = {"PGPASSWORD": password}
+        proc = subprocess.Popen(
+            command,
+            env=env,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE)
         set_subprocess_stdout_and_stderr_nonblocking(proc)
         self.pid = proc.pid
         self.log.info("Started: %r, running as PID: %r", command, self.pid)
