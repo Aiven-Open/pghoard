68ee1518e3e499e71959597bb174e21b22ef2123
Will Rouesnel <willrouesnel@aiven.io>
2020-08-25 23:36:00 +1000

    pghoard: Type ignore for monitoring/__init__.py magic

--- pghoard/monitoring/__init__.py
+++ pghoard/monitoring/__init__.py
@@ -1,5 +1,6 @@
 import pkgutil
 
-__path__ = pkgutil.extend_path(__path__, __name__)
+__path__ = pkgutil.extend_path(__path__, __name__)  # type: ignore
+
 for importer, modname, ispkg in pkgutil.walk_packages(path=__path__, prefix=__name__ + "."):
     __import__(modname)
