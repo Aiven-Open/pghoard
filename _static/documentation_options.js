fdda61d150e9a3815cda8ccf69df2a736dd1b5a8
Kevin <80679746+kmichel-aiven@users.noreply.github.com>
2022-12-06 16:25:20 +0000

    From refs/heads/main af7efbcb

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.1-0-unknown-6a29d01',
+    VERSION: '0.0.1-0-unknown-af7efbc',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
