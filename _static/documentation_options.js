0be6932655460c7fb94fa2c0587acabd51d0deca
Kevin <80679746+kmichel-aiven@users.noreply.github.com>
2023-05-04 13:02:34 +0000

    From refs/heads/main b2b8465f

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.1-0-unknown-0d177de',
+    VERSION: '0.0.1-0-unknown-b2b8465',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
