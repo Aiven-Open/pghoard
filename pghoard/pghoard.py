db82d1a19fa8c2c79796942e52a4c36156375a35
Kathia Barahona <kathia.barahona@aiven.io>
2023-07-04 09:07:19 +0000

    improved log messages

--- pghoard/pghoard.py
+++ pghoard/pghoard.py
@@ -637,14 +637,15 @@ class PGHoard:
                         os.unlink(full_path)
                     else:
                         delete_request = WalFileDeletionEvent(backup_site_name=site, file_path=Path(full_path))
+                        self.log.info("Adding an Uncompressed WAL file to deletion queue: %s", full_path)
                         self.wal_file_deletion_queue.put(delete_request)
-                        self.log.info("Adding to Uncompressed WAL file to deletion queue: %s", full_path)
                     continue
 
                 # delete compressed file and re-try
                 if is_already_compressed and not has_metadata_file:
                     self.log.info(
-                        "Deleting invalid compressed file %r, compression will be re-tried", compressed_file_path,
+                        "Deleting incomplete compressed file %r (missing metadata), compression will be re-tried",
+                        compressed_file_path,
                     )
                     os.unlink(compressed_file_path)
 
